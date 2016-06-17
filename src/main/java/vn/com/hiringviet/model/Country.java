46127c5d91a8513badddf63da6c4ebfa5574a12b
A6terabytenta <ntatvr@gmail.com>
2016-06-17 07:47:25 +0700

    upload class ComemntDTO

---
+++ src/main/java/vn/com/hiringviet/model/Country.java
@@ -0,0 +1,53 @@
+package vn.com.hiringviet.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "country")
+public class Country implements Serializable {
+
+	private static final long serialVersionUID = 3431890485282466969L;
+
+	private int id;
+
+	private String countryName;
+
+	private String flag;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "country_name")
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	@Column(name = "flag")
+	public String getFlag() {
+		return flag;
+	}
+
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
+
+	
+}
